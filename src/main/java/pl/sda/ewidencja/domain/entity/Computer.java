d01872837b09edb1dd371b2ecdf129aaf9e1cb7a
Sławek S <slaavii@gmail.com>
2018-11-17 14:21:19 +0100

    Ustawiam encje cd..

--- src/main/java/pl/sda/ewidencja/domain/entity/Computer.java
+++ src/main/java/pl/sda/ewidencja/domain/entity/Computer.java
@@ -2,75 +2,75 @@ package pl.sda.ewidencja.domain.entity;
 
 import javax.persistence.*;
 
+@Entity
 public class Computer {
-    @Entity
-    public class NetDevice {
-        @Id
-        @GeneratedValue(strategy = GenerationType.AUTO)
-        private Long id;
-        @Enumerated(EnumType.STRING)
-        private Enum typ;
-        private String marka;
-        private String serialNumber;
-        private String operatingSystem;
-        private String localization;
-        private String ipAddress;
 
-        public Long getId() {
-            return id;
-        }
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    @Enumerated(EnumType.STRING)
+    private Enum typ;
+    private String marka;
+    private String serialNumber;
+    private String operatingSystem;
+    private String localization;
+    private String ipAddress;
 
-        public Enum getTyp() {
-            return typ;
-        }
-
-        public String getMarka() {
-            return marka;
-        }
-
-        public String getSerialNumber() {
-            return serialNumber;
-        }
-
-        public String getOperatingSystem() {
-            return operatingSystem;
-        }
-
-        public String getLocalization() {
-            return localization;
-        }
-
-        public String getIpAddress() {
-            return ipAddress;
-        }
-
-        public void setId(Long id) {
-            this.id = id;
-        }
-
-        public void setTyp(Enum typ) {
-            this.typ = typ;
-        }
-
-        public void setMarka(String marka) {
-            this.marka = marka;
-        }
-
-        public void setSerialNumber(String serialNumber) {
-            this.serialNumber = serialNumber;
-        }
-
-        public void setOperatingSystem(String operatingSystem) {
-            this.operatingSystem = operatingSystem;
-        }
-
-        public void setLocalization(String localization) {
-            this.localization = localization;
-        }
-
-        public void setIpAddress(String ipAddress) {
-            this.ipAddress = ipAddress;
-        }
+    public Long getId() {
+        return id;
     }
 
+    public Enum getTyp() {
+        return typ;
+    }
+
+    public String getMarka() {
+        return marka;
+    }
+
+    public String getSerialNumber() {
+        return serialNumber;
+    }
+
+    public String getOperatingSystem() {
+        return operatingSystem;
+    }
+
+    public String getLocalization() {
+        return localization;
+    }
+
+    public String getIpAddress() {
+        return ipAddress;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public void setTyp(Enum typ) {
+        this.typ = typ;
+    }
+
+    public void setMarka(String marka) {
+        this.marka = marka;
+    }
+
+    public void setSerialNumber(String serialNumber) {
+        this.serialNumber = serialNumber;
+    }
+
+    public void setOperatingSystem(String operatingSystem) {
+        this.operatingSystem = operatingSystem;
+    }
+
+    public void setLocalization(String localization) {
+        this.localization = localization;
+    }
+
+    public void setIpAddress(String ipAddress) {
+        this.ipAddress = ipAddress;
+    }
 }
+
+
