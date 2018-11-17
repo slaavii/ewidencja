30c52a01608be53f759f6bb5359cd8e010133eb7
Paweł Błaszczuk <pawel.blaszczuk@gmail.com>
2018-11-17 13:01:55 +0100

    pierwszy comit

---
+++ src/main/java/pld/sda/ewidencja/EwidencjaApplication.java
@@ -0,0 +1,12 @@
+package pld.sda.ewidencja;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EwidencjaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EwidencjaApplication.class, args);
+	}
+}
