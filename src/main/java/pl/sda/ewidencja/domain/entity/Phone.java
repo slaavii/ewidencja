c5a4df83f838a212c3040cd63f1a7fb2c5a7a982
Paweł Błaszczuk <pawel.blaszczuk@gmail.com>
2018-11-17 14:33:24 +0100

    drugi commit

--- src/main/java/pl/sda/ewidencja/domain/entity/Phone.java
+++ src/main/java/pl/sda/ewidencja/domain/entity/Phone.java
@@ -1,4 +1,64 @@
 package pl.sda.ewidencja.domain.entity;
 
+import javax.persistence.*;
+
+@Entity
 public class Phone {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    @Enumerated(EnumType.STRING)
+    Enum typ;
+    String marka;
+    String serialNumber;
+    String localization;
+    String imei;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Enum getTyp() {
+        return typ;
+    }
+
+    public void setTyp(Enum typ) {
+        this.typ = typ;
+    }
+
+    public String getMarka() {
+        return marka;
+    }
+
+    public void setMarka(String marka) {
+        this.marka = marka;
+    }
+
+    public String getSerialNumber() {
+        return serialNumber;
+    }
+
+    public void setSerialNumber(String serialNumber) {
+        this.serialNumber = serialNumber;
+    }
+
+    public String getLocalization() {
+        return localization;
+    }
+
+    public void setLocalization(String localization) {
+        this.localization = localization;
+    }
+
+    public String getImei() {
+        return imei;
+    }
+
+    public void setImei(String imei) {
+        this.imei = imei;
+    }
 }
