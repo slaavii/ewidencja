878ffa21550eca550fd6815676df42503271a716
Sławek S <slaavii@gmail.com>
2018-11-18 17:23:31 +0100

    Updejty w klasach ...

--- src/main/java/pl/sda/ewidencja/controller/EmployeeController.java
+++ src/main/java/pl/sda/ewidencja/controller/EmployeeController.java
@@ -1,10 +1,26 @@
 package pl.sda.ewidencja.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
+import pl.sda.ewidencja.service.EmployeeService;
 
 @Controller
-@RequestMapping("/employees")
 public class EmployeeController {
+
+    private final EmployeeService service;
+
+    @Autowired
+    public EmployeeController(EmployeeService service) {
+        this.service = service;
+    }
+
+    @GetMapping(value = "/employee")
+    public ModelAndView costsPage() {
+        ModelAndView mav = new ModelAndView("employee");
+        mav.addObject("employee", service.getAll());
+        return mav;
+    }
 
 }
