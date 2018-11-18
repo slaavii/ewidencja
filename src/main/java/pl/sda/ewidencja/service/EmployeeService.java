a6060d3080d11eb10899703455b8a722ff040c83
Sławek S <slaavii@gmail.com>
2018-11-18 10:32:03 +0100

    Powstają kontrolery ...

--- src/main/java/pl/sda/ewidencja/service/EmployeeService.java
+++ src/main/java/pl/sda/ewidencja/service/EmployeeService.java
@@ -1,7 +1,13 @@
 package pl.sda.ewidencja.service;
 
 import org.springframework.stereotype.Service;
+import pl.sda.ewidencja.repository.ComputerRepository;
+import pl.sda.ewidencja.repository.EmployeeRepository;
 
 @Service
 public class EmployeeService {
+    private final ComputerRepository computerRepository;
+    private final EmployeeRepository employeeRepository;
+    private
+
 }
